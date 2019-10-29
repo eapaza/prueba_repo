77c0874b341ce1e3e05716099251fbae94fe96a9
Erick Gonzalo Apaza Espinoza <gapaza.espinoza@gmail.com>
2019-10-28 20:10:50 -0400

    funciones bbdd

---
+++ bbdd/funciones_bbdd.php
@@ -0,0 +1,42 @@
+<?php
+
+	function getConexion()
+	{
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$database = 'test_db';
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $database);
+
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		return $conn;
+	}
+	
+	function consultar($sql)
+	{
+		$conn = getConexion();
+		$stmt = $conn->query($sql);	
+		$resultado = array();
+		while( $row = mysqli_fetch_array( $stmt, MYSQLI_ASSOC) ) {
+			$resultado[] = $row;
+		}
+		
+		return $resultado;
+	}
+	
+	function insertar($id, $descripcion)
+	{
+		$conn = getConexion();
+		$stmt = $conn->query("insert into prueba values ({$id}, '{$descripcion}')");
+		if ( !$stmt ) {
+			return false;
+		}
+		return true;
+	}
+?>
---
+++ bbdd/procesar_bbdd.php
@@ -0,0 +1,148 @@
+<?php
+	require('funciones_bbdd.php');
+	$msg = '';
+	$conn = getConexion();
+	$id = '';
+	$descripcion = '';
+	$opeaux = '';
+	
+	// modo spagetti
+	/*if (isset($_REQUEST['id'])) {
+		if (isset($_REQUEST['ope'])){
+			$stmt = $conn->query("SELECT * FROM prueba where id = {$_REQUEST['id']}");
+			while( $row = mysqli_fetch_array( $stmt, MYSQLI_ASSOC) ) {
+				$id = $row['id'];
+				$descripcion = $row['descripcion'];
+			}			
+			$opeaux = 'mod';
+		} else {
+			if ($_REQUEST['opeaux'] == 'mod') {
+				if (isset($_REQUEST['descripcion'])) {
+					$stmt = $conn->query("update prueba set descripcion = '{$_REQUEST['descripcion']}' where id = {$_REQUEST['id']}");
+					if ( !$stmt ) {
+						$msg = 'Error en el update';
+					}
+				} else {
+					$msg = 'Es necesario descripcion';
+				}
+			} else {
+				if (isset($_REQUEST['descripcion'])) {
+					$stmt = $conn->query("insert into prueba values ({$_REQUEST['id']}, '{$_REQUEST['descripcion']}')");
+					if ( !$stmt ) {
+						$msg = 'Error en el insert';
+					}
+				} else {
+					$msg = 'Es necesario descripcion';
+				}
+			}
+		}
+	}
+	
+	$stmt = $conn->query("SELECT * FROM prueba");
+	$tablaDetalle = '';
+	while( $row = mysqli_fetch_array( $stmt, MYSQLI_ASSOC) ) {
+		$tablaDetalle .= "<tr><td>{$row['id']}</td><td>{$row['descripcion']}</td><td><a href='procesar_bbdd?id={$row['id']}&ope=upd'>editar</a></td><tr>";
+		//$tablaDetalle = $tablaDetalle . "<tr><td>{$row['id']}</td><td>{$row['descripcion']}</td><tr>";
+	}
+	$tabla =  "<table border='1'>
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>DESCRIPCION</td>
+					<td>ACCIONES</td>
+				</tr>
+			</thead>
+			<tbody>
+				{$tablaDetalle}
+			</tbody>
+		  </table>";
+	*/
+	
+	// modo optimo
+	
+	$res_ins = insertar($_REQUEST['id'], $_REQUEST['descripcion']);
+	
+	if (!$res_ins){
+		$msg = 'Error en el insert';
+	} else {
+		$msg = 'Insert correcto!!';
+	}
+	
+	$datos = consultar("SELECT * FROM prueba");
+	$tablaDetalle = '';
+	foreach($datos as $row){
+		$tablaDetalle .= "<tr><td>{$row['id']}</td><td>{$row['descripcion']}</td><td><a href='procesar_bbdd?id={$row['id']}&ope=upd'>editar</a></td><tr>";
+	}
+	$tabla =  "<table border='1'>
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>DESCRIPCION</td>
+					<td>ACCIONES</td>
+				</tr>
+			</thead>
+			<tbody>
+				{$tablaDetalle}
+			</tbody>
+		  </table>";
+	
+	
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="UTF-8">
+     <title>CRUD</title>
+     <style>
+        input {
+        width: 250px;
+        padding: 5px;
+     }
+     .redondeado {
+       border-radius: 5px;
+     }
+     .confondo {
+       background-color: #def;
+     }
+     .sinborde {
+        border: 0;
+     }
+      .sinbordefondo {
+       background-color: #eee;
+       border: 0;
+       size: "200";
+     }
+    .outlinenone {
+    outline: none;
+    background-color: #dfe;
+    border: 0;
+     }
+  .redondeadonorelieve {
+    border-radius: 5px;
+    border: 1px solid #39c;
+     }
+    </style>
+    </head>
+	<body>
+		<h2>ABM BASE DE DATOS</h2>
+		<form method="post" action="procesar_bbdd.php">
+		  <br>
+		     <!--<input type="text" name="mensaje" style="font-family: Arial; size: 300; font-size: 20pt; background-color: #eee" readOnly="true" class="sinbordefondo" value="<?php echo $msg; ?>" >-->
+		     <input type="text" name="mensaje" readOnly="true" class="sinbordefondo" value="<?php echo $msg; ?>" >
+		  <br>
+		  ID:<br>
+		  <input type="number" name="id" value="<?php echo $id; ?>" >
+		  <br>
+		  DESCRIPCION:<br>
+		  <input type="string" name="descripcion" value="<?php echo $descripcion; ?>" >
+		  <input type="hidden" name="opeaux" value="<?php echo $opeaux; ?>">
+		  <br>
+		  <input type="submit" value="GUARDAR">
+		</form>
+		<br>
+		<br>
+		<?php echo $tabla; ?>
+		
+	</body>
+</html>
